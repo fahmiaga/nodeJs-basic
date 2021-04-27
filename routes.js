19b7f6039f4c79c1418e93b0b768912383fc3c82
Fahmi Aga <fahmi.aga@gmail.com>
2021-04-27 10:09:18 +0700

    make crud

---
+++ routes.js
@@ -0,0 +1,42 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+  // getBookByQueryHandler,
+} = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/books",
+    handler: addBookHandler,
+  },
+  {
+    method: "GET",
+    path: "/books",
+    handler: getAllBooksHandler,
+  },
+  {
+    method: "GET",
+    path: "/books/{id}",
+    handler: getBookByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/books/{id}",
+    handler: editBookByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/books/{id}",
+    handler: deleteBookByIdHandler,
+  },
+  // {
+  //   method: "GET",
+  //   path: "/books{name?}",
+  //   handler: getBookByQueryHandler,
+  // },
+];
+module.exports = routes;
